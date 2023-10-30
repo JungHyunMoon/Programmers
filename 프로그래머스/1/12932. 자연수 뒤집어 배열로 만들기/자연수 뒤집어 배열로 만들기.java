48887d6e20e4f248a3a1aa537c18b370b405b595
문정현 <120004247+JungHyunMoon@users.noreply.github.com>
2023-10-30 16:09:46 +0900

    [level 1] Title: 자연수 뒤집어 배열로 만들기, Time: 0.08 ms, Memory: 71.7 MB -BaekjoonHub

--- 프로그래머스/1/12932. 자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.java
+++ 프로그래머스/1/12932. 자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.java
@@ -1,32 +1,16 @@
 class Solution {
-    public long[] solution(long n) {
-//         long count = 0;
-//         for (long i = n; i > 0; i/=10){
-//             count++;
-//         }
-//         long[] answer = new long[(int)count];
-//         String temp = String.valueOf(n);
-        
-//         for (int i = 0; i < answer.length; i++){
-//             answer[i] = Long.valueOf(temp.substring((int)(count-1), (int)count));
-//             count--;
-//         }
-//         return answer;
-                int size = String.valueOf(n).length();
-        long[] answer = new long[size];
-
-        String[] list = String.valueOf(n).split("");
-        for (int i = 0; i < list.length/2; i++) {
-            int oppositeIndex = size - 1 - i;
-            String opposite = list[oppositeIndex];
-            list[oppositeIndex] = list[i];
-            list[i] = opposite;
+    public static long[] solution(long n) {
+        long count = 0;
+        for (long i = n; i > 0; i/=10){
+            count++;
         }
+        long[] answer = new long[(int)count];
+        String temp = String.valueOf(n);
 
-        for (int i = 0; i < list.length; i++) {
-            answer[i] = Long.parseLong(list[i]);
+        for (int i = 0; i < answer.length; i++){
+            answer[i] = Long.valueOf(temp.substring((int)(count-1), (int)count));
+            count--;
         }
-
         return answer;
     }
 }
