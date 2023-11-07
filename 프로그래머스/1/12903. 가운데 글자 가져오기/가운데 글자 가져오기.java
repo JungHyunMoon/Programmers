41d16df6cd5868c51336d24cf724a37e0f1e9626
문정현 <120004247+JungHyunMoon@users.noreply.github.com>
2023-11-08 01:31:23 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 9.11 ms, Memory: 77.5 MB -BaekjoonHub

--- 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.java
+++ 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.java
@@ -1,13 +1,30 @@
 class Solution {
     public String solution(String s) {
         String answer = "";
-        int center = s.length() / 2; // 홀수 5 면 2.5에 버림 2 => 중앙값
-        if (s.length() % 2 == 0) {
-            answer += s.charAt(center - 1);
-            answer += s.charAt(center);
-        } else {
-            answer += s.charAt(center);
+        // int center = s.length() / 2; // 홀수 5 면 2.5에 버림 2 => 중앙값
+        // if (s.length() % 2 == 0) {
+        //     answer += s.charAt(center - 1);
+        //     answer += s.charAt(center);
+        // } else {
+        //     answer += s.charAt(center);
+        // }
+        // return answer;
+        
+        int size = s.length();
+        int start = (size - 1) / 2;
+        int end = size / 2;
+        if (start != end) {
+            answer += s.charAt(start);
         }
+
+        answer += s.charAt(end);
         return answer;
+        
+        // 5
+        // 1로 빼고 나누면 2
+        // 2로 나누면 2.5
+        // 4
+        // 1로 빼고 나누면 1
+        // 2로 나누면 2
     }
 }
