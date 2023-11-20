d50661ddb7c889d633c56f6fb5606b7d1f04dbde
문정현 <120004247+JungHyunMoon@users.noreply.github.com>
2023-11-21 04:10:12 +0900

    [level 1] Title: 2016년, Time: 0.90 ms, Memory: 79.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/12901. 2016년/2016년.java
@@ -0,0 +1,10 @@
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+
+class Solution {
+    public String solution(int a, int b) {
+        LocalDate date = LocalDate.of(2016, a, b);
+        DayOfWeek dayOfWeek = date.getDayOfWeek();
+        return dayOfWeek.toString().substring(0,3);
+    }
+}
